45a18ae074b761a8c39ec84b2e00a91a158e354f
Alex Gottschalk <alexgottschalk2013@gmail.com>
2021-11-21 14:43:14 +0200

    restart

--- API/index.js
+++ API/index.js
@@ -1,7 +1,32 @@
-const app = require('express')();
-const PORT = 8080;
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const PORT = 8080
+
+const app = express()
+
+const articles = []
+
+app.get('/news', (req, res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch(err => console.log(err))
+
+})
 
 app.listen(
     PORT,
-    () => console.log(`it's alive on http://localhost:${PORT}`
+    () => console.log(`server running on http://localhost:${PORT}`
 ));
