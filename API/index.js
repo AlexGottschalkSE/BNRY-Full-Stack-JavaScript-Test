cd806aa5a30a37d4b526f630321ee7a982323ae0
Alex Gottschalk <alexgottschalk2013@gmail.com>
2021-11-21 16:41:45 +0200

    Added /search /headlines and /bitcoin API requests

--- API/index.js
+++ API/index.js
@@ -1,32 +1,48 @@
 const express = require('express')
 const axios = require('axios')
-const cheerio = require('cheerio')
+const { json } = require('express')
 const PORT = 8080
-
 const app = express()
 
-const articles = []
-
-app.get('/news', (req, res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
-        .then((response) => {
-            const html = response.data
-            const $ = cheerio.load(html)
-
-            $('a:contains("climate")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                articles.push({
-                    title,
-                    url
-                })
-            })
-            res.json(articles)
+//Handle /news API request
+app.get('/headlines', (req, res) => {
+    //Make API call to news API
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`)
+        .then((newsAPIResponse) => {
+            //Return news API response
+            res.send(newsAPIResponse.data)
         }).catch(err => console.log(err))
+})
 
+//Handle /bitcoin API request
+app.get('/bitcoin', (req, res) => {
+    //Make API call to news API
+    axios.get(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${process.env.NEWS_API_KEY}`)
+        .then((newsAPIResponse) => {
+            //Return news API response
+            teststring = newsAPIResponse.data
+            res.send(newsAPIResponse.data)
+        }).catch(err => console.log(err)
+        );
+})
+
+//Handle /search API request
+app.get('/search/:q', (req, res) => {
+    //Make API call to news API
+    axios.get(`https://newsapi.org/v2/everything?q=${req.params.q}&apiKey=${process.env.NEWS_API_KEY}`)
+        .then((newsAPIResponse) => {
+            //Return news API response
+            res.send(newsAPIResponse.data)
+        }).catch(err => console.log(err))
+})
+
+//Handle / API request
+app.get('/', (req, res) => {
+    //Make API call to news API
+    res.send({ "Result": "Endpoint unhandled" })
 })
 
 app.listen(
     PORT,
     () => console.log(`server running on http://localhost:${PORT}`
-));
+    ));
