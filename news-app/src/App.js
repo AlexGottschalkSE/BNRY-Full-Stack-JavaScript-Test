6d8c922d970950edbd6827295975cfaf760b4404
Alex Gottschalk <alexgottschalk2013@gmail.com>
2021-11-22 19:49:13 +0200

    Adding Buttons to Frontend

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,16 +1,21 @@
-import React, { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   return <div className='app'>
+
     <button onClick={() => {
       fetch('http://localhost:8080/bitcoin')
         .then((res) => res.json())
         .then(console.log);
-    }}>Display Data
+    }}>Display Bitcoin Data
+    </button>
+
+    <button onClick={() => {
+      fetch('http://localhost:8080/headlines')
+        .then((res) => res.json())
+        .then(console.log);
+    }}>Display Top Headlines
     </button>
   </div>
 }
-
-
 export default App;
